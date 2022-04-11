b5ca5412a6cb2e1916f4f67f9bac0267f04c4fff
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-04-11 12:11:48 -0400

    commit tree 9265cfded32bf24db65f5d0c04d09eb36f9318eb

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,48 @@
 # your code here
+require 'pry'
+class Stack
+
+  def initialize(limit=nil)
+    @stack = []
+    @limit = limit
+  end
+
+  def push(value)
+    if self.full?
+      raise 'Stack Overflow'
+    else
+      @stack.push(value)
+    end
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+    @stack.last
+  end
+
+  def size
+    @stack.size
+  end
+
+  def empty?
+    @stack.size == 0
+  end
+
+  def full?
+    @limit && @stack.size == @limit
+  end
+
+  def search(value)
+    if @stack.include?(value)
+      @stack.size - (@stack.index(value) + 1)
+    else
+      -1
+    end
+  end
+
+end
+
+# binding.pry
